9fc036d5818e90d51bb9238d286433346dd13d0a
profsain <profsaincrown@gmail.com>
2018-09-02 21:25:07 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am Profsain Hussain.</p>
+    <p>Good to Go! FreeCodeCamp Hackathon 2018,</p>
+    <Link to="/page-2/">Go to Next</Link>
+  </div>
+)
+
+export default IndexPage
